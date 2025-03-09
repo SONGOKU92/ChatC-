1bcb2754834450a4a84856b54c81ecbf3dd88ed2
SuperBG <157978359+SONGOKU92@users.noreply.github.com>
2025-03-09 14:39:18 +0100

    first commit

---
+++ Chat/Form1.Designer.cs
@@ -0,0 +1,16 @@
+﻿namespace ChatServerGUI
+{
+    partial class ServerForm
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        private System.ComponentModel.IContainer components = null;
+
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        #region Code généré par le Concepteur Windows Form
+
+
+        #endregion
+    }
+}
+
---
+++ ChatClient/Form1.Designer.cs
@@ -0,0 +1,16 @@
+﻿namespace ChatClientGUI
+{
+    partial class ClientForm
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+    }
+}
+
